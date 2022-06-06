0a098302aa1f9c4b1ef95d0bda57cc1466db07fd
Juan Garrido <juangj1b@gmail.com>
2022-06-06 11:29:20 +0200

    Tutorial Angular - S12 84 - Creación del proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { TableroComponent } from './componentes/tablero/tablero.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CabeceraComponent,
+    TableroComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
