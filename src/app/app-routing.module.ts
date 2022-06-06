3ee3fe5bb3b041e21b17d86aab1a52dc6cffe833
Juan Garrido <juangj1b@gmail.com>
2022-06-06 12:34:04 +0200

    Tutorial Angular - S12 87 - Enrutamientos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConfiguracionComponent } from './componentes/configuracion/configuracion.component';
+import { EditarClienteComponent } from './componentes/editar-cliente/editar-cliente.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { NotFoundComponent } from './componentes/not-found/not-found.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { TableroComponent } from './componentes/tablero/tablero.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TableroComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegistroComponent},
+  {path: 'configuracion', component: ConfiguracionComponent},
+  {path: 'cliente/editar/:id', component: EditarClienteComponent},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
