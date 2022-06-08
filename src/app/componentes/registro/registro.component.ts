b9dd4926f8df0e02c6c6b39e4b5462c02f305621
Juan Garrido <juangj1b@gmail.com>
2022-06-08 14:42:11 +0200

    Tutorial Angular - S13 106 - Registro de usuarios de Firestore

--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'flash-messages-angular';
+import { LoginService } from 'src/app/servicios/login.service';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
+  styleUrls: ['./registro.component.css'],
 })
 export class RegistroComponent implements OnInit {
+  email: string;
+  password: string;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private flashMessages: FlashMessagesService,
+    private loginService: LoginService
+  ) {}
 
   ngOnInit(): void {
+    this.loginService.getAuth().subscribe(auth => {
+      if(auth){
+        this.router.navigate(['/']);
+      }
+    })
   }
 
+  register(){
+    this.loginService.register(this.email, this.password)
+      .then( res => {
+        this.router.navigate(['/'])
+      })
+      .catch(error => {
+        this.flashMessages.show( error.message, {
+          cssClass: 'alert-danger', timeout: 4000
+        });
+      })
+  }
 }
--- src/app/servicios/login.service.ts
+++ src/app/servicios/login.service.ts
@@ -3,25 +3,32 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { map } from 'rxjs/operators';
 
 @Injectable()
-export class LoginService{
-    constructor(private authService: AngularFireAuth){}
+export class LoginService {
+  constructor(private authService: AngularFireAuth) {}
 
-    login(email: string, password: string){
-        return new Promise((resolve, reject) => {
-            this.authService.signInWithEmailAndPassword(email, password)
-            .then(datos => resolve(datos),
-                error => reject(error)
-            )
-        })
-    }
+  login(email: string, password: string) {
+    return new Promise((resolve, reject) => {
+      this.authService.signInWithEmailAndPassword(email, password).then(
+        (datos) => resolve(datos),
+        (error) => reject(error)
+      );
+    });
+  }
 
-    getAuth(){
-        return this.authService.authState.pipe(
-            map( auth => auth)
-        );
-    }
+  getAuth() {
+    return this.authService.authState.pipe(map((auth) => auth));
+  }
 
-    logout(){
-        this.authService.signOut();
-    }
+  logout() {
+    this.authService.signOut();
+  }
+
+  register(email: string, password: string) {
+    return new Promise((resolve, reject) => {
+      this.authService.createUserWithEmailAndPassword(email, password).then(
+        (datos) => resolve(datos),
+        (error) => reject(error)
+      );
+    });
+  }
 }
