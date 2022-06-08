46911be0c84e92ff2019b046c69a20326d5a80be
Juan Garrido <juangj1b@gmail.com>
2022-06-08 10:13:38 +0200

    Tutorial Angular - S12 99 - Componente Modificar (Solo HTML)

--- src/app/componentes/editar-cliente/editar-cliente.component.ts
+++ src/app/componentes/editar-cliente/editar-cliente.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FlashMessagesService } from 'flash-messages-angular';
+import { Cliente } from 'src/app/modelo/cliente.model';
+import { ClienteServicio } from 'src/app/servicios/cliente.service';
 
 @Component({
   selector: 'app-editar-cliente',
   templateUrl: './editar-cliente.component.html',
-  styleUrls: ['./editar-cliente.component.css']
+  styleUrls: ['./editar-cliente.component.css'],
 })
 export class EditarClienteComponent implements OnInit {
+  cliente: Cliente = {
+    nombre: '',
+    apellido: '',
+    email: '',
+    saldo: 0,
+  };
 
-  constructor() { }
+  id: string;
+
+  constructor(
+    private clientesServicio: ClienteServicio,
+    private flashMessages: FlashMessagesService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.clientesServicio.getCliente(this.id).subscribe((cliente) => {
+      this.cliente = cliente;
+    });
   }
 
+  guardar(form : NgForm){
+
+  }
+
+  eliminar(){
+    
+  }
 }
