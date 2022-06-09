53b4e7ac2e4f866a9262d56d0ce56aa8f08e0267
Juan Garrido <juangj1b@gmail.com>
2022-06-09 09:14:22 +0200

    Tutorial Angular - S13 109 - Mostrar/Ocultar el Registro de la cabecera según la configuración

--- src/app/componentes/cabecera/cabecera.component.ts
+++ src/app/componentes/cabecera/cabecera.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ConfiguracionService } from 'src/app/servicios/configuracion service';
 import { LoginService } from 'src/app/servicios/login.service';
 
 @Component({
@@ -10,8 +11,13 @@ import { LoginService } from 'src/app/servicios/login.service';
 export class CabeceraComponent implements OnInit {
   isLoggedIn: boolean;
   loggedInUser: string | null;
+  permitirRegistro: any;
 
-  constructor(private loginService: LoginService, private router: Router) {}
+  constructor(
+    private loginService: LoginService,
+    private router: Router,
+    private configuracionServicio: ConfiguracionService
+  ) {}
 
   ngOnInit(): void {
     let auth = this.loginService.getAuth().subscribe((auth) => {
@@ -22,6 +28,12 @@ export class CabeceraComponent implements OnInit {
         this.isLoggedIn = false;
       }
     });
+
+    this.configuracionServicio
+      .getConfiguracion()
+      .subscribe((configuracion) => {
+        this.permitirRegistro = configuracion.permitirRegistro;
+      });
   }
 
   logout() {
