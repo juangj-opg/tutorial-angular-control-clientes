140d47803163599ab10fb25b4fbac63e08845e3c
Juan Garrido <juangj1b@gmail.com>
2022-06-09 08:50:17 +0200

    Tutorial Angular - S13 107 - Componente de configuración - HTML

--- src/app/componentes/configuracion/configuracion.component.ts
+++ src/app/componentes/configuracion/configuracion.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-configuracion',
   templateUrl: './configuracion.component.html',
-  styleUrls: ['./configuracion.component.css']
+  styleUrls: ['./configuracion.component.css'],
 })
 export class ConfiguracionComponent implements OnInit {
+  permitirRegistro = false;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  guardar(){
+    
   }
-
 }
